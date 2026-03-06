fc426e82ae6b3dbf597276feed409d355ae25759
Fabian Ibarra <fabiluisibarra@gmail.com>
2026-03-05 21:19:49 -0300

    Update Select.js

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,14 +5,39 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+// Parametros: value (valor seleccionado actualmente), onchange (funcion que se ejecuta si el usuario, elige opcion distinta)
 const Select = ({ label, value, onChange, children }) => {
+  // busca entre los hijos, similitud con el valor actual
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <SelectWrapper>
+    <Nativeselect value={value} onChange={onChange}>
       {children}
-    </select>
+    </Nativeselect>
+    <Helper>hello<Icon id="chevron-down" size={24} strokeWidth={2} /></Helper>
+    </SelectWrapper>
   );
 };
 
+
+const SelectWrapper= styled.div `
+position: relative;
+width: max-content;
+`
+const Helper= styled.div `
+background-color:${COLORS.transparentGray15};
+color:${COLORS.gray700};
+padding: 12px 16px;
+font-size: 16px;
+`
+
+const Nativeselect= styled.select `
+position: absolute;
+top: 0;
+left: 0;
+width: 100%;
+height: 100%;
+opacity: 0;
+`
 export default Select;
